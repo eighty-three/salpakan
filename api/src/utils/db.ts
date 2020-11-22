c28908ba122616e37b6dd83ef72071206b2a85e0
techiesspammer <techiesspammer@protonmail.com>
2020-11-22 17:38:33 +1300

    export player pool list name

--- api/src/utils/db.ts
+++ api/src/utils/db.ts
@@ -1,8 +1,10 @@
 import config from '../utils/config';
 import pgp from 'pg-promise';
+import * as redis from 'redis';
 
 const pg = pgp();
 const cn = `postgresql://${config.DB_USERNAME}:${config.DB_PASSWORD}@localhost:${config.DB_PORT}/${config.DB_PROJNAME}`;
-const db = pg(cn);
+export const db = pg(cn);
 
-export default db;
+export const PLAYER_POOL_LIST_NAME = config.REDIS_PLAYER_POOL_LIST
+export const redisClient = redis.createClient(parseInt(config.REDIS_PORT!));
