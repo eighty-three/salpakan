9a320fa47e54ec1c72fb8d6fb9554ac56ef1cbb8
techiesspammer <techiesspammer@protonmail.com>
2020-11-22 17:36:26 +1300

    update config to include redis player pool list name

--- api/src/utils/config.ts
+++ api/src/utils/config.ts
@@ -8,7 +8,9 @@ const config = {
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_PROJNAME: process.env.DB_PROJNAME,
   DB_PORT: process.env.DB_PORT,
-  SECRET_JWT: process.env.SECRET_JWT as string
+  SECRET_JWT: process.env.SECRET_JWT as string,
+  REDIS_PORT: process.env.REDIS_PORT,
+  REDIS_PLAYER_POOL_LIST: process.env.REDIS_PLAYER_POOL_LIST ? "player_pool" : process.env.REDIS_PLAYER_POOL_LIST as string,
 };
 
 export default config;
