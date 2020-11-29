81fee968dc15900660eaf96e194c716226beee4e
Eighty Three <cwp1@protonmail.com>
2020-11-29 15:02:10 +0800

    Add 2 hours lifespan to game

--- api/src/game/model.ts
+++ api/src/game/model.ts
@@ -23,11 +23,13 @@ export const startGame = async (
     time: null
   };
 
+  const expiry = Math.floor(Date.now() / 1000) + 7200;
+
   const query = new PS({ name: 'start-game', text: '\
-    INSERT INTO games (name, player1, player2) VALUES ($1, $2, $3)'
+    INSERT INTO games (name, player1, player2, expiry) VALUES ($1, $2, $3, $4)'
   });
 
-  query.values= [ roomName, ...arr ];
+  query.values= [ roomName, ...arr, expiry ];
   await db.none(query);
 };
 
@@ -35,10 +37,12 @@ export const getGame = async (
   roomName: string
 ): Promise<any|null> => {
   const query = new PS({ name: 'get-game', text: '\
-    SELECT FROM games WHERE name=$1'
+    SELECT FROM games WHERE name=$1 AND expiry > $2'
   });
 
-  query.values = [ roomName ];
+  const currentTime = Math.floor(Date.now() / 1000);
+
+  query.values = [ roomName, currentTime ];
   return await db.oneOrNone(query);
 };
 
@@ -54,5 +58,16 @@ export const storeGame = async (
   });
 
   query.values = [ roomName, player1_state, player2_state, false ];
+  await db.none(query);
+};
+
+export const deleteGames = async (): Promise<void> => {
+  const query = new PS({ name: 'delete-games', text: '\
+    DELETE FROM games WHERE expiry < $1'
+  });
+
+  const currentTime = Math.floor(Date.now() / 1000);
+
+  query.values = [ currentTime ];
   await db.none(query);
 };
