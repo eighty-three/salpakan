c3a9b622a123522d18d3653d5861471d002eb945
Eighty Three <cwp1@protonmail.com>
2020-11-29 21:08:28 +0800

    Fix conditional statement flow

--- api/src/game/controller.ts
+++ api/src/game/controller.ts
@@ -5,16 +5,16 @@ export const getGame: RequestHandler = async (req, res) => {
   const roomName = req.query.id as string;
   const gameState: any = await game.getGame(roomName);
 
-  if (!gameState) {
-    res.json({ error: 'Game not found' });
-  } else if (gameState.ongoing) {
+  if (gameState?.ongoing) {
     res.json({ ongoing: true });
-  } else {
+  } else if (gameState) {
     res.json({
       player1_state: gameState.player1_state,
       player2_state: gameState.player2_state,
       player1: gameState.player1,
       player2: gameState.player2
     });
+  } else {
+    res.json({ error: 'Game not found' });
   }
 };
