3d30f27e5a882241f410b5cbb4c75daf4b4c2339
Eighty Three <cwp1@protonmail.com>
2020-12-12 10:20:28 +0800

    Change setTimeout interval from 100 to 92.5
    If it's set to 100, it's too slow compared to the time from the server.
    Every 15 seconds, it lags behind an extra second compared to server time.
    At 90, the client countdown becomes too fast. At 92.5, it gets delayed
    by a second every 1 minute and 30 seconds. Until I can come up with a
    more exact method to count the time without constantly badgering the
    server (unless it's not that big of a deal?) this is sufficient because
    if you're playing the game "legitimately", there's no way you'll spend
    more than 30 seconds, or maybe even 20 seconds each turn.

--- client/src/components/Game/Countdown.jsx
+++ client/src/components/Game/Countdown.jsx
@@ -19,11 +19,22 @@ const Countdown = (props) => {
 
   const timeOut = useRef(null);
   useEffect(() => {
-    timeOut.current = workerTimers.setTimeout(counter, 100);
+    timeOut.current = workerTimers.setTimeout(counter, 92.5);
     return () => workerTimers.clearTimeout(timeOut.current);
   }, [time, turn]);
   /* `turn` dependency included for MatchClocks. Still works
    * as intended in SetupPanel because it'll be null there
+   *
+   * The interval is set at 92.5 because if it's set to 100, it's too slow
+   * compared to the time from the server. Every 15 seconds, it lags behind
+   * an extra second compared to server time. At 90, the client countdown
+   * becomes too fast. At 92.5, it gets delayed by a second every 1 minute
+   * and 30 seconds. Until I can come up with a more exact method to count
+   * the time without constantly badgering the server (unless it's not that
+   * big of a deal?) this is sufficient because if you're playing the game
+   * "legitimately", there's no way you'll spend more than 30 seconds, or
+   * maybe even 20 seconds each turn. Let's just say that the game is meant
+   * to be played fast. "It's not a bug, it's a feature"
    */
 
   const currentTime = new Date(time * 100);
