1507024055caee3aeb363869e33001445ef3b832
Eighty Three <contact@eighty-three.dev>
2020-12-16 15:53:56 +0800

    Add WS_PORT var in env file

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
-    PORT: process.env.PORT,
     HOST: process.env.HOST,
+    PORT: process.env.PORT,
+    WS_PORT: process.env.WS_PORT,
     SECRET: process.env.SECRET
   },
   trailingSlash: true
