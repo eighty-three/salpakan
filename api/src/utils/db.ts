40d1233839bea8f42ec1a175d7f68964031cef82
techiesspammer <techiesspammer@protonmail.com>
2020-12-29 18:20:06 +1300

    [sqm] apply pr changes: use try catch and iife

--- api/src/utils/db.ts
+++ api/src/utils/db.ts
@@ -6,24 +6,22 @@ const pg = pgp();
 const cn = `postgresql://${config.DB_USERNAME}:${config.DB_PASSWORD}@${config.DB_HOSTNAME}:${config.DB_PORT}/${config.DB_PROJNAME}`;
 const db = pg(cn);
 
-// tests connection and returns Postgres server version,
+// tests connection and prints out Postgres server version,
 // if successful; or else rejects with connection error:
-let dbConnection: any;
-db.connect()
-.then(obj => {
-  dbConnection = obj;
-  logger.info(`successfully connected to postgres database ${config.DB_PROJNAME}.
-  server version: ${dbConnection.client.serverVersion}
-  port: ${config.PORT} host: ${config.DB_HOSTNAME}.`);
-}).catch(error => {
-  logger.error(`error connecting to postgres database ${config.DB_PROJNAME} error: ${error}`)
-}).finally(()=> {
-  // release the connection, if it was successful:
-  if (dbConnection) {
-    // if you pass `true` into method done, i.e. done(true),
-    // it will make the pool kill the physical connection.
-    dbConnection.done();
+(async () => {
+  let dbConnection;
+  try {
+    dbConnection = await db.connect();
+    logger.info(`successfully connected to postgres database ${config.DB_PROJNAME}.
+    server version: ${dbConnection.client.serverVersion}
+    port: ${config.PORT} host: ${config.DB_HOSTNAME}.`);  }
+    catch (err) {
+      logger.error(`error connecting to postgres database ${config.DB_PROJNAME} error: ${err}`)
+    } finally {
+    if (dbConnection) {
+      dbConnection.done();
+    }
   }
-});
+})();
 
 export default db;
