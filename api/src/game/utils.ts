fc60d3a86b536f308b7f5953879ee8bf6b65ddcc
Eighty Three <contact@eighty-three.dev>
2021-01-04 09:07:57 +0800

    perf: unroll the loop
    Instead of doing a for loop, I don't know why I shouldn't just return
    these objects instead, considering it won't be changing anyway. It's
    also not terribly huge for me to be mindful of the memory space

--- api/src/game/utils.ts
+++ api/src/game/utils.ts
@@ -1,48 +1,57 @@
 import { IBoard, TPlayer } from './types';
 
-const pieces = [
-  { name: 'spy', value: 99 },
-  { name: 'spy', value: 99 },
-  { name: 'flag', value: 1 },
-  { name: 'private', value: 2 },
-  { name: 'private', value: 2 },
-  { name: 'private', value: 2 },
-  { name: 'private', value: 2 },
-  { name: 'private', value: 2 },
-  { name: 'private', value: 2 },
-  { name: 'sergeant', value: 3 },
-  { name: '2ndlt', value: 4 },
-  { name: '1stlt', value: 5 },
-  { name: 'captain', value: 6 },
-  { name: 'major', value: 7 },
-  { name: 'ltcol', value: 8 },
-  { name: 'col', value: 9 },
-  { name: '1star', value: 10 },
-  { name: '2star', value: 11 },
-  { name: '3star', value: 12 },
-  { name: '4star', value: 13 },
-  { name: '5star', value: 14 }
-];
-
 export const getInitialBoardState = (player: TPlayer): IBoard => {
-  const board: IBoard = {};
-  const rows = (player === 'p1')
-    ? ['A', 'B', 'C']
-    : ['I', 'H', 'G'];
+  const player1Board = {
+    A1: { name: 'spy', value: 99 },
+    A2: { name: 'spy', value: 99 },
+    A3: { name: 'flag', value: 1 },
+    A4: { name: 'private', value: 2 },
+    A5: { name: 'private', value: 2 },
+    A6: { name: 'private', value: 2 },
+    A7: { name: 'private', value: 2 },
+    A8: { name: 'private', value: 2 },
+    B1: { name: 'private', value: 2 },
+    B2: { name: 'sergeant', value: 3 },
+    B3: { name: '2ndlt', value: 4 },
+    B4: { name: '1stlt', value: 5 },
+    B5: { name: 'captain', value: 6 },
+    B6: { name: 'major', value: 7 },
+    B7: { name: 'ltcol', value: 8 },
+    B8: { name: 'col', value: 9 },
+    C1: { name: '1star', value: 10 },
+    C2: { name: '2star', value: 11 },
+    C3: { name: '3star', value: 12 },
+    C4: { name: '4star', value: 13 },
+    C5: { name: '5star', value: 14 }
+  };
 
-  for (let i = 0; i < pieces.length; i++) {
-    let x;
-    const y = `${i % 8 + 1}`;
-    if (i < 8) {
-      x = rows[0];
-    } else if (i < 16) {
-      x = rows[1];
-    } else if (i < 24) {
-      x = rows[2];
-    }
+  const player2Board = {
+    I1: { name: 'spy', value: 99 },
+    I2: { name: 'spy', value: 99 },
+    I3: { name: 'flag', value: 1 },
+    I4: { name: 'private', value: 2 },
+    I5: { name: 'private', value: 2 },
+    I6: { name: 'private', value: 2 },
+    I7: { name: 'private', value: 2 },
+    I8: { name: 'private', value: 2 },
+    H1: { name: 'private', value: 2 },
+    H2: { name: 'sergeant', value: 3 },
+    H3: { name: '2ndlt', value: 4 },
+    H4: { name: '1stlt', value: 5 },
+    H5: { name: 'captain', value: 6 },
+    H6: { name: 'major', value: 7 },
+    H7: { name: 'ltcol', value: 8 },
+    H8: { name: 'col', value: 9 },
+    G1: { name: '1star', value: 10 },
+    G2: { name: '2star', value: 11 },
+    G3: { name: '3star', value: 12 },
+    G4: { name: '4star', value: 13 },
+    G5: { name: '5star', value: 14 }
+  };
 
-    board[x + y] = pieces[i];
+  if (player === 'p1') {
+    return player1Board;
+  } else {
+    return player2Board;
   }
-
-  return board;
 };
