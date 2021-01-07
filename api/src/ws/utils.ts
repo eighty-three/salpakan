e7070e049dc9bd161b7dd0da8b067b6daf713913
Eighty Three <contact@eighty-three.dev>
2021-01-07 18:31:41 +0800

    refactor: put functions into utils.ts

--- api/src/ws/utils.ts
+++ api/src/ws/utils.ts
@@ -1,4 +1,34 @@
+import { IRoom, TPlayer } from '../game/types';
+import { performance } from 'perf_hooks';
 import { StringDecoder } from 'string_decoder';
+
 const decoder = new StringDecoder('utf8');
 
 export const decode = (buf: ArrayBuffer): string => decoder.write(Buffer.from(buf));
+
+export const refreshTime = (room: IRoom, player: TPlayer) => {
+  const currentTime = performance.now() / 100;
+  const elapsedTime = currentTime - room.lastMove;
+  room[player].time = room[player].time - elapsedTime;
+  room.lastMove = performance.now() / 100;
+
+  if (room[player].time <= 0) {
+    const opponent = (player === 'p1') ? 'p2' : 'p1';
+    room.winner = room[opponent].name;
+  }
+};
+
+export const getGameInfo = (room: IRoom) => {
+  return {
+    turn: room.turn,
+    p1: {
+      name: room.p1.name,
+      time: room.p1.time
+    },
+    p2: {
+      name: room.p2.name,
+      time: room.p2.time
+    },
+    winner: room.winner
+  };
+};
