67e1a190234af24ade4b3b53a97b087e829b44a5
Eighty Three <contact@eighty-three.dev>
2021-01-15 08:49:27 +0800

    fix: prevent user from passing fake coordinates
    checkIfLegal is already in the client. It'll be added to the server too just
    in case the user managed to send fake coordinates like 'X1' or something
    like 'A1' as the origin but 'C5' as the destination. The checks are
    still done in the client as a way of preventing the user from repeatedly
    sending bogus moves to the server (reducing unnecessary hits)

--- api/src/game/utils.ts
+++ api/src/game/utils.ts
@@ -86,6 +86,56 @@ export const cleanBoards = (
 
 /**
  * checks two coordinates, e.g., `A1` and `B1`, to know if
+ * movement between them is possible
+ *
+ * @param {TCoordinate} origin The coordinate where the move comes from
+ * @param {TCoordinate} destination The coordinate where the move is going to
+ * @returns {boolean} true if legal, false otherwise
+ */
+export const checkIfLegal = (
+  origin: TCoordinate,
+  destination: TCoordinate
+): boolean => {
+  try {
+    const originRow = origin.charCodeAt(0);
+    const destRow = destination.charCodeAt(0);
+    const originCol = Number(origin[1]);
+    const destCol = Number(destination[1]);
+
+    /* If not within bounds then return false. Coordinates are from
+     * A1 to I8, 'A'.charCodeAt() is 65, 'I'.charCodeAt() is 73,
+     * so adding both origin and destination should always be within
+     * 130-146 range, as is the case with the number part, with 2-16.
+     * It can go through if the client somehow sends something like
+     * 'A15' to 'A1' but it will be taken into account in the next
+     * conditional statements, where it counts how many steps it took
+     * from origin to destination
+     */
+    if (
+      !((originRow + destRow >= 130 && originRow + destRow <= 146)
+      && (originCol + destCol >= 2 && originCol + destCol <= 16))
+    ) return false;
+
+    /* If row changed just one step and if column stayed in place,
+     * or vice versa, then return true. A piece can only move vertically
+     * or horizontally one step at a time, e.g., A1 to B1, or A1 to A2.
+     * Going from A1 to A3 or A1 to C1 is impossible
+     */
+    if (
+      ((Math.abs(originRow - destRow) === 1) && (originCol - destCol === 0))
+      || ((originRow - destRow === 0) && (Math.abs(originCol - destCol) === 1))
+    ) {
+      return true;
+    }
+
+    return false;
+  } catch {
+    return false;
+  }
+};
+
+/**
+ * checks two coordinates, e.g., `A1` and `B1`, to know if
  * movement between them is possible. It should be legal and the
  * pieces should be in the possession of the correct player
  *
