045bebed29edf0ce38a3ae48a858f741b134331d
Eighty Three <contact@eighty-three.dev>
2021-01-16 17:36:09 +0800

    feat: add 'getCookie' function
    The function will run in the homepage if the user is not logged in or if
    the user doesn't have a previous cookie (given by the function).
    'withAuthGSSP' is modified to look for the '=' character in order to
    determine of the cookie is from logging in or is just from the
    'getCookie' function. Normally, 'decrypting' the token inside the cookie
    will just return the username. However, if it's the cookie from the
    function, its output will instead be a 15-character string prefixed by
    the characters 'c=', in order to differentiate it.

--- api/src/account/controller.ts
+++ api/src/account/controller.ts
@@ -4,6 +4,7 @@ import config from '@utils/config';
 import * as argon2 from 'argon2';
 import { sign } from 'jsonwebtoken';
 import cookie from 'cookie';
+import { nanoid } from 'nanoid';
 
 import { createAccount } from './model';
 
@@ -12,6 +13,7 @@ export const login: RequestHandler = async (req, res) => {
 
   const claims = { username };
   const authToken = sign(claims, config.SECRET_JWT, { expiresIn: '1y' });
+
   res.setHeader('Set-Cookie', cookie.serialize('auth', authToken, {
     httpOnly: true,
     secure: config.NODE_ENV !== 'development',
@@ -30,6 +32,7 @@ export const signup: RequestHandler = async (req, res) => {
 
   const claims = { username };
   const authToken = sign(claims, config.SECRET_JWT, { expiresIn: '1y' });
+
   res.setHeader('Set-Cookie', cookie.serialize('auth', authToken, {
     httpOnly: true,
     secure: config.NODE_ENV !== 'development',
@@ -45,4 +48,19 @@ export const logout: RequestHandler = async (req, res) => {
   const { message } = req.body;
   res.clearCookie('auth', { path: '/' });
   res.status(200).json({ message });
+};
+
+export const getCookie: RequestHandler = async (req, res) => {
+  const claims = { username: `c=${nanoid(15)}` };
+
+  const authToken = sign(claims, config.SECRET_JWT, { expiresIn: '1y' });
+  res.setHeader('Set-Cookie', cookie.serialize('auth', authToken, {
+    httpOnly: true,
+    secure: config.NODE_ENV !== 'development',
+    sameSite: 'strict',
+    maxAge: 31536000,
+    path: '/',
+  }));
+
+  res.status(200).json({ message: 'Cookie set' });
 };
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
 
@@ -7,15 +7,29 @@ import Layout, { siteTitle } from '@/components/Layout';
 import MatchmakingComponent from '@/components/MatchmakingComponent';
 
 import { lightAuthCheck } from '@/lib/authCheck';
+import { getCookie } from '@/lib/account';
 
 const propTypes = {
-  username: PropTypes.string
+  username: PropTypes.string,
+  userId: PropTypes.string
 };
 
 const Home = (props) => {
   const {
-    username
+    username,
+    userId
   } = props;
+
+  useEffect(() => {
+    if (!userId) {
+      const setCookie = async () => {
+        await getCookie();
+      };
+
+      setCookie();
+    }
+  }, []);
+
 
   return (
     <Layout username={username}>
@@ -30,12 +44,14 @@ const Home = (props) => {
 };
 
 export const getServerSideProps = async (ctx) => {
-  const username = await lightAuthCheck(ctx);
+  const userId = await lightAuthCheck(ctx);
+  const username = (userId && userId[1] !== '=') ? userId : null;
 
   return {
     props:
       {
-        username
+        username,
+        userId
       }
   };
 };
