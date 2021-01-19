80adfcdb5d92c92eba50f10ecdd60fc75ebd4d08
Eighty Three <contact@eighty-three.dev>
2021-01-19 10:04:10 +0800

    feat: add css

--- client/src/components/Game/Board/Piece.jsx
+++ client/src/components/Game/Board/Piece.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styles from './Piece.module.css';
+
 const propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
+  coordinate: PropTypes.string
 };
 
 const Piece = (props) => {
   const {
-    name
+    name,
+    coordinate
   } = props;
+  // useState to save coordinate?
+
+  const pieceStyle = styles[name]; // should have 'unknown' style to represent unknown pieces
+  const coordinateStyle = styles[coordinate];
+  const css = `${pieceStyle} ${coordinateStyle} ${styles.piece}`;
 
   return (
-    <div>
-      {name}
-    </div>
+    <>
+      <div className={styles.wrapper}>
+        <div className={css}>
+          {name}{coordinate}
+        </div>
+      </div>
+    </>
   );
 };
 
