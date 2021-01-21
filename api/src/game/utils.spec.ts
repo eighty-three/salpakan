0ea7c3abd9f3109dd1bd0e1e1b8a444d5f35d553
techiesspammer <techiesspammer@protonmail.com>
2021-01-21 22:48:21 +1300

    refactor utils.spec.ts
    - add test for movePiece()
    - add test for checkMove()

--- api/src/game/utils.spec.ts
+++ api/src/game/utils.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable */
-import { checkIfLegal, checkMove, cleanBoards } from './utils';
+import { checkMove, cleanBoards, movePiece } from './utils';
 import { IGameStates } from './types';
 
 describe('testing checkIfLegal', () => {
@@ -27,73 +27,34 @@ describe('testing checkIfLegal', () => {
  * I can't be arsed to fix it in here though since it's irrelevant to the function
  */
 
-describe('testing checkMove', () => {
+describe('checkMove', () => {
   const gameStates: IGameStates = {};
+  const roomName = 'testRoom';
 
-  describe('test basics', () => {
-    const roomName = 'testroom1';
-
-    const p1Board = {
-      A1: { name: 'x', value: 1 },
-      A2: { name: 'x', value: 1 }
-    };
-
-    const p2Board = {};
-
-    const { board1, board2, bothBoards } = cleanBoards(p1Board, p2Board);
-
-    gameStates[roomName] = {
-      playerList: ['p1', 'p2'], turn: 'p1', start: true, lastMove: 1, time: 1, winner: null,
-      board: {...bothBoards},
-      p1: {
-        time: 1, start: true, name: 'p1',
-        board: {...board1}
-      },
-      p2: {
-        time: 1, start: true, name: 'p2',
-        board: {...board2}
-      }
-    };
-
-    test('to be 0, origin not owned by player', () => {
-      expect(checkMove(gameStates, roomName, 'p1', 'B1', 'B2')).toBe(0);
-    });
-
-    test('to be 0, destination is owned by player', () => {
-      expect(checkMove(gameStates, roomName, 'p1', 'A1', 'A2')).toBe(0);
-    });
-
-    test('to be 1, player has the piece', () => {
-      expect(gameStates[roomName].p1.board['A1']).toStrictEqual({name: 'x', value: 1});
-      expect(gameStates[roomName].p1.board['B1']).toBe(undefined);
-
-      expect(checkMove(gameStates, roomName, 'p1', 'A1', 'B1')).toBe(1);
-
-      // piece has moved
-      expect(gameStates[roomName].p1.board['A1']).toBe(undefined);
-      expect(gameStates[roomName].p1.board['B1']).toStrictEqual({name: 'x', value: 1});
-    });
-  });
-
-  describe('test vs', () => {
-    const roomName = 'testroom2';
-
+  beforeEach(() => {
     const p1Board = {
       A1: { name: 'x', value: 1 },
       A2: { name: 'x', value: 1 },
-      A3: { name: 'x', value: 99 },
-      A4: { name: 'x', value: 5 },
-      A5: { name: 'x', value: 5 },
-      A6: { name: 'x', value: 2 }
+      A5: { name: 'x', value: 99 },
+      B3: { name: 'x', value: 5 },
+      B5: { name: 'x', value: 5 },
+      C2: { name: 'x', value: 5 },
+      D3: { name: 'x', value: 5 },
+      H5: { name: 'x', value: 2 },
+      I5: { name: 'x', value: 2 }
     };
 
     const p2Board = {
-      B1: { name: 'x', value: 1 },
-      B2: { name: 'x', value: 2 },
-      B3: { name: 'x', value: 2 },
-      B4: { name: 'x', value: 4 },
-      B5: { name: 'x', value: 10 },
-      B6: { name: 'x', value: 99 }
+      A6: { name: 'x', value: 2 },
+      A8: { name: 'x', value: 1 },
+      A7: { name: 'x', value: 2 },
+      B6: { name: 'x', value: 2 },
+      D6: { name: 'x', value: 2 },
+      E6: { name: 'x', value: 4 },
+      F5: { name: 'x', value: 4 },
+      H6: { name: 'x', value: 2 },
+      H8: { name: 'x', value: 10 },
+      I6: { name: 'x', value: 99 }
     };
 
     const { board1, board2, bothBoards } = cleanBoards(p1Board, p2Board);
@@ -110,57 +71,80 @@ describe('testing checkMove', () => {
         board: {...board2}
       }
     };
+  });
 
-    test('to be 3, result draw', () => {
-      expect(checkMove(gameStates, roomName, 'p1', 'A1', 'B1')).toBe(3);
+  describe('pre-requisites', () => {
+    test('it returns 0 when the origin is owned by the opponent', () => {
+      expect(checkMove(gameStates, roomName, 'p1', 'A8', 'A7')).toEqual(0);
     });
 
-    test('to be 2, d > o', () => {
+    test('it returns 0 when the destination is owned by the player', () => {
+      expect(checkMove(gameStates, roomName, 'p1', 'A1', 'A2')).toEqual(0);
+    });
+  });
+
+  describe('piece movement', () => {
+    test('it returns 3 (draw) when o === d', () => {
+      expect(checkMove(gameStates, roomName, 'p1', 'H5', 'H6')).toEqual(3);
+    });
+
+    test('edge case: it returns 2 when d > o, given that d and o are at either ends of the board', () => {
       /* Example of impossible move in the client but valid in server.
        * Only for testing purposes, it should normally be impossible
        */
-      expect(checkMove(gameStates, roomName, 'p1', 'A2', 'B2')).toBe(2);
+      expect(checkMove(gameStates, roomName, 'p1', 'A2', 'I6')).toEqual(2);
     });
 
-    test('to be 2, attacker is spy, target is private', () => {
-      expect(checkMove(gameStates, roomName, 'p1', 'A3', 'B3')).toBe(2);
+    test('it returns 2 when attacker is spy, target is a private', () => {
+      expect(checkMove(gameStates, roomName, 'p1', 'A5', 'A6')).toEqual(2);
     });
 
-    test('to be 1, o > d', () => {
-      expect(checkMove(gameStates, roomName, 'p1', 'A4', 'B4')).toBe(1);
+    test('it returns 2, attacker is spy, target is not a private', () => {
+      expect(checkMove(gameStates, roomName, 'p1', 'A5', 'A6')).toEqual(2);
     });
 
-    test('to be 1, o > d, empty space', () => {
-      expect(checkMove(gameStates, roomName, 'p1', 'A5', 'C1')).toBe(1);
+    test('it returns 1 when o > d', () => {
+      expect(checkMove(gameStates, roomName, 'p1', 'B5', 'B6')).toEqual(1);
     });
 
-    test('to be 1, attacker is private, target is spy', () => {
-      expect(checkMove(gameStates, roomName, 'p1', 'A6', 'B6')).toBe(1);
+    test('it returns 1 when o > d, where d is an empty space', () => {
+      expect(checkMove(gameStates, roomName, 'p1', 'D3', 'D4')).toEqual(1);
+    });
+
+    test('it returns 1 when attacker is private, target is spy', () => {
+      expect(checkMove(gameStates, roomName, 'p1', 'I5', 'I6')).toEqual(1);
+    });
+
+    test('it returns 2 when attacker is private, target is not a spy', () => {
+      expect(checkMove(gameStates, roomName, 'p1', 'I5', 'F5')).toEqual(2);
     });
   });
+});
 
-  describe('test side effects', () => {
-    const roomName = 'testroom3';
+describe('movePiece', () => {
+  const gameStates: IGameStates = {};
+  const roomName = 'testroom4';
 
+  beforeEach(()=> {
     const p1Board = {
-      A1: { name: 'x', value: 1 },
-      A2: { name: 'x', value: 1 },
-      A3: { name: 'x', value: 99 },
-      A4: { name: 'x', value: 5 },
-      A5: { name: 'x', value: 5 },
-      A6: { name: 'x', value: 2 }
+      A1: { name: 'foo', value: 1 },
+      A2: { name: 'bar', value: 1 },
+      A3: { name: 'baz', value: 99 },
+      A4: { name: 'moo', value: 5 },
+      A5: { name: 'cat', value: 5 },
+      A6: { name: 'sta', value: 2 }
     };
 
     const p2Board = {
-      B1: { name: 'x', value: 1 },
-      B2: { name: 'x', value: 2 },
-      B3: { name: 'x', value: 2 },
-      B4: { name: 'x', value: 4 },
-      B5: { name: 'x', value: 10 },
-      B6: { name: 'x', value: 99 }
+      B1: { name: 'bar', value: 1 },
+      B2: { name: 'bar', value: 2 },
+      B3: { name: 'bar', value: 2 },
+      B4: { name: 'bar', value: 4 },
+      B5: { name: 'bar', value: 10 },
+      B6: { name: 'bar', value: 99 }
     };
 
-    const { board1, board2, bothBoards } = cleanBoards(p1Board, p2Board);
+    const {board1, board2, bothBoards} = cleanBoards(p1Board, p2Board);
 
     gameStates[roomName] = {
       playerList: ['p1', 'p2'], turn: 'p1', start: true, lastMove: 1, time: 1, winner: null,
@@ -174,46 +158,79 @@ describe('testing checkMove', () => {
         board: {...board2}
       }
     };
-
-    // re side effects, see utils.ts lines 197-222
   });
 
-  describe('test modified boards', () => {
-    const roomName = 'testroom4';
+  describe('when result is 1', () => {
+    test('it updates all boards with the destination, and deletes the origin.', () => {
+      movePiece(
+        gameStates[roomName].board,
+        gameStates[roomName].p1.board,
+        gameStates[roomName].p2.board,
+        'A2',
+        'A3',
+        1);
+      expect(gameStates[roomName].p1.board['A3']).toEqual({ name: 'bar', value: 1 });
+      expect(gameStates[roomName].p2.board['A3']).toEqual({ name: 'unknown'});
 
-    const p1Board = {
-      A1: { name: 'x', value: 1 },
-      A2: { name: 'x', value: 1 },
-      A3: { name: 'x', value: 99 },
-      A4: { name: 'x', value: 5 },
-      A5: { name: 'x', value: 5 },
-      A6: { name: 'x', value: 2 }
-    };
+      expect(gameStates[roomName].p1.board['A2']).toBeUndefined();
+      expect(gameStates[roomName].p2.board['A2']).toBeUndefined();
+      expect(gameStates[roomName].board['A2']).toBeUndefined();
+    });
+    test('it sets the destination value to \'undefined\' when origin doesnt exist.', () => {
+      movePiece(
+        gameStates[roomName].board,
+        gameStates[roomName].p1.board,
+        gameStates[roomName].p2.board,
+        'A7',
+        'A6',
+        1);
 
-    const p2Board = {
-      B1: { name: 'x', value: 1 },
-      B2: { name: 'x', value: 2 },
-      B3: { name: 'x', value: 2 },
-      B4: { name: 'x', value: 4 },
-      B5: { name: 'x', value: 10 },
-      B6: { name: 'x', value: 99 }
-    };
+      expect(gameStates[roomName].p1.board['A7']).toBeUndefined();
+      expect(gameStates[roomName].p2.board['A7']).toBeUndefined();
+    });
+  });
+  describe('when result is 3', () => {
+    test('it deletes the destination and origin from all boards.', () => {
+      movePiece(
+        gameStates[roomName].board,
+        gameStates[roomName].p1.board,
+        gameStates[roomName].p2.board,
+        'A2',
+        'A3',
+        3);
 
-    const { board1, board2, bothBoards } = cleanBoards(p1Board, p2Board);
+      expect(gameStates[roomName].p1.board['A2']).toBeUndefined();
+      expect(gameStates[roomName].p2.board['A2']).toBeUndefined();
+      expect(gameStates[roomName].board['A2']).toBeUndefined();
 
-    gameStates[roomName] = {
-      playerList: ['p1', 'p2'], turn: 'p1', start: true, lastMove: 1, time: 1, winner: null,
-      board: {...bothBoards},
-      p1: {
-        time: 1, start: true, name: 'p1',
-        board: {...board1}
-      },
-      p2: {
-        time: 1, start: true, name: 'p2',
-        board: {...board2}
-      }
-    };
+      expect(gameStates[roomName].p1.board['A3']).toBeUndefined();
+      expect(gameStates[roomName].p2.board['A3']).toBeUndefined();
+      expect(gameStates[roomName].board['A3']).toBeUndefined();
+    });
+  });
+  describe('when result is 2', () => {
+    test('it deletes the origin from all boards.', () => {
+      movePiece(
+        gameStates[roomName].board,
+        gameStates[roomName].p1.board,
+        gameStates[roomName].p2.board,
+        'A2',
+        'A3',
+        2);
 
-    // re modified boards, see utils.ts lines 227-242
+      expect(gameStates[roomName].p1.board['A2']).toBeUndefined();
+      expect(gameStates[roomName].p2.board['A2']).toBeUndefined();
+      expect(gameStates[roomName].board['A2']).toBeUndefined();
+    });
+
+    test('it doesnt throw an error when the origin doesnt exist', () => {
+      expect(movePiece(
+        gameStates[roomName].board,
+        gameStates[roomName].p1.board,
+        gameStates[roomName].p2.board,
+        'A7',
+        'A3',
+        2)).not.toThrowError;
+    });
   });
 });
