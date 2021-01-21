0302e87b4d933e87abb93437d7853584f039bb04
Eighty Three <contact@eighty-three.dev>
2021-01-22 01:38:31 +0800

    feat: add event listeners to 'Piece'

--- client/src/components/Game/Board/Piece.jsx
+++ client/src/components/Game/Board/Piece.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 
-import styles from './Piece.module.css';
+import styles from './Piece.module.scss';
+
+import DragContext from '@/lib/DragContext';
 
 const propTypes = {
   name: PropTypes.string,
@@ -10,26 +12,55 @@ const propTypes = {
 
 const Piece = (props) => {
   const {
-    name,
-    coordinate
+    name, // for image of piece
+    coordinate // for position of piece
   } = props;
-  // useState to save coordinate?
 
-  const pieceStyle = styles[name]; // should have 'unknown' style to represent unknown pieces
+  /* `setVis` is separate from `dispatch` from context
+   * because it should be local to the piece, else
+   * every other piece will also be hidden on movable
+   */
+  const [ vis, setVis ] = useState(null);
+  const [state, dispatch] = useContext(DragContext);
+
+  const dragStart = (e) => {
+    e.preventDefault();
+
+    /* get coordinate from localStorage because using
+     * dataTransfer.(g|s)etData isn't working
+     */
+    localStorage.setItem('coordinate', coordinate);
+
+    setVis('hidden');
+    dispatch({ type: 'dragStart' });
+  };
+
+  const dragEnd = (e) => {
+    e.preventDefault();
+
+    setVis('visible');
+    dispatch({ type: 'dragEnd' });
+  };
+
+  const pieceStyle = styles[name];
   const coordinateStyle = styles[coordinate];
   const css = `${pieceStyle} ${coordinateStyle} ${styles.piece}`;
 
-  return (
-    <>
-      <div className={styles.wrapper}>
-        <div className={css}>
-          {name}{coordinate}
-        </div>
-      </div>
-    </>
-  );
+  const options = (name !== 'unknown' && !state.winner)
+    ? {
+      className: `${css} ${styles.movable}`,
+      onDragEnter: dragStart,
+      onDragEnd: dragEnd,
+      draggable: state.draggable,
+      style: { visibility: vis }
+    } : {
+      className: css,
+    };
+
+  const div = React.createElement('div', options);
+
+  return div;
 };
 
 Piece.propTypes = propTypes;
-
 export default Piece;
