3591b1923a3086e392a8a6c578c686071fd62180
Eighty Three <contact@eighty-three.dev>
2021-01-22 10:18:02 +0800

    feat: add reducer and context for dragging
    Toggles the CSS of DragTarget and the draggable state of the
    pieces. It also has an 'update' case for the winner

---
+++ client/src/lib/DragReducer.js
@@ -0,0 +1,31 @@
+import styles from '@/components/Game/Board/DragTarget.module.scss';
+
+const TurnChangeReducer = (state, action) => {
+  switch (action.type) {
+    case 'dragStart': {
+      return {
+        draggable: false,
+        css: `${styles.target} ${styles.top}`,
+        winner: state.winner
+      };
+    }
+
+    case 'dragEnd': {
+      return {
+        draggable: true,
+        css: styles.target,
+        winner: state.winner
+      };
+    }
+
+    case 'update': {
+      return {
+        draggable: (action.payload) ? false : true,
+        css: styles.target,
+        winner: action.payload
+      };
+    }
+  }
+};
+
+export default TurnChangeReducer;
