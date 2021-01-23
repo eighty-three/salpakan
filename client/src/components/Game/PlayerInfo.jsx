905bf7a9001e5461c2ecaa5e4bb3b8e5263b2ac3
Eighty Three <contact@eighty-three.dev>
2021-01-24 00:58:09 +0800

    feat: prevent rerendering of PlayerInfo
    useContext and other relevant lines were moved up to the parent
    component (Player), passing them down as props instead in order to make
    React.memo work

--- client/src/components/Game/PlayerInfo.jsx
+++ client/src/components/Game/PlayerInfo.jsx
@@ -1,30 +1,18 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './PlayerInfo.module.css';
 
-import GameInfoContext from '@/lib/GameInfoContext';
-import PlayerContext from '@/lib/PlayerContext';
-
 const propTypes = {
-  playerNum: PropTypes.string
+  name: PropTypes.string,
+  css: PropTypes.string
 };
 
 const PlayerInfo = (props) => {
   const {
-    playerNum
+    name,
+    css
   } = props;
-
-  const gameInfo = useContext(GameInfoContext);
-  const player = useContext(PlayerContext);
-
-  const name = (gameInfo?.[playerNum].name[1] !== '=')
-    ? gameInfo?.[playerNum].name
-    : 'Anonymous';
-
-  const css = (player === playerNum)
-    ? `${styles.player_text} ${styles.self}`
-    : styles.player_text;
 
   return (
     <div className={styles.container}>
@@ -35,4 +23,4 @@ const PlayerInfo = (props) => {
 
 PlayerInfo.propTypes = propTypes;
 
-export default PlayerInfo;
+export default React.memo(PlayerInfo);
