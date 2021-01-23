a1268f23be107bd60e700c73eccbec9138e5151e
Eighty Three <contact@eighty-three.dev>
2021-01-23 17:56:03 +0800

    build: fix webpack to allow sound imports

--- client/next.config.js
+++ client/next.config.js
@@ -7,5 +7,21 @@ module.exports = withPreact({
     WS_PORT: process.env.WS_PORT,
     SECRET: process.env.SECRET
   },
-  trailingSlash: true
+  trailingSlash: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/sounds/',
+          outputPath: 'static/sounds/',
+          name: '[name].[ext]',
+          esModule: false,
+        },
+      },
+    });
+
+    return config;
+  },
 });
