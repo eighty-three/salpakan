1e5d455ffc3312d11f08841fcfe5212ccad722e7
Eighty Three <contact@eighty-three.dev>
2021-01-24 01:42:22 +0800

    fix: prevent all Piece components from rerendering
    Previously, on each move, every Piece component, even the ones not
    involved in the "move", were rerendering. Instead of relying on a state
    from the parent component (Pieces), a state 'dragging' was set in the
    Piece component that would replace the behaviour of the previous
    'draggable' state from Pieces.
    The useContext hook was also removed in favor of simply passing down the
    dispatch and the relevant state (winner) as props, removing DragContext
    altogether

--- client/src/components/Game/Board/Piece.jsx
+++ client/src/components/Game/Board/Piece.jsx
@@ -1,30 +1,29 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './Piece.module.scss';
 
-import DragContext from '@/lib/DragContext';
-
 const propTypes = {
   name: PropTypes.string,
-  coordinate: PropTypes.string
+  coordinate: PropTypes.string,
+  updateDragState: PropTypes.func,
+  winner: PropTypes.bool
 };
 
 const Piece = (props) => {
   const {
     name, // for image of piece
-    coordinate // for position of piece
+    coordinate, // for position of piece
+    updateDragState,
+    winner
   } = props;
 
-  /* `setVis` is separate from `dispatch` from context
-   * because it should be local to the piece, else
-   * every other piece will also be hidden on movable
-   */
   const [ vis, setVis ] = useState(null);
-  const [state, dispatch] = useContext(DragContext);
+  const [ isDragging, setIsDragging ] = useState(false);
 
   const dragStart = (e) => {
     e.preventDefault();
+    setIsDragging(true);
 
     /* get coordinate from localStorage because using
      * dataTransfer.(g|s)etData isn't working
@@ -32,26 +31,29 @@ const Piece = (props) => {
     localStorage.setItem('coordinate', coordinate);
 
     setVis('hidden');
-    dispatch({ type: 'dragStart' });
+    updateDragState({ type: 'dragStart' });
   };
 
   const dragEnd = (e) => {
-    e.preventDefault();
+    if (isDragging) {
+      e.preventDefault();
 
-    setVis('visible');
-    dispatch({ type: 'dragEnd' });
+      setIsDragging(false);
+      setVis('visible');
+      updateDragState({ type: 'dragEnd' });
+    }
   };
 
   const pieceStyle = styles[name];
   const coordinateStyle = styles[coordinate];
   const css = `${pieceStyle} ${coordinateStyle} ${styles.piece}`;
 
-  const options = (name !== 'unknown' && !state.winner)
+  const options = (name !== 'unknown' && !winner)
     ? {
       className: `${css} ${styles.movable}`,
       onDragEnter: dragStart,
       onDragEnd: dragEnd,
-      draggable: state.draggable,
+      draggable: true,
       style: { visibility: vis }
     } : {
       className: css,
@@ -63,4 +65,4 @@ const Piece = (props) => {
 };
 
 Piece.propTypes = propTypes;
-export default Piece;
+export default React.memo(Piece);
