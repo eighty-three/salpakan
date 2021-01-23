b7eb769d53f313db57d19a9cf7f1f80772e5d19a
Eighty Three <contact@eighty-three.dev>
2021-01-24 00:58:09 +0800

    feat: prevent rerendering of PlayerInfo
    useContext and other relevant lines were moved up to the parent
    component (Player), passing them down as props instead in order to make
    React.memo work

--- client/src/components/Game/Player.jsx
+++ client/src/components/Game/Player.jsx
@@ -2,12 +2,14 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './Player.module.css';
+import playerInfoStyle from './PlayerInfo.module.css';
 import MatchClock from './MatchClock';
 import PlayerInfo from './PlayerInfo';
 import Winner from './Winner';
 
 import GameInfoContext from '@/lib/GameInfoContext';
 import TurnContext from '@/lib/TurnContext';
+import PlayerContext from '@/lib/PlayerContext';
 
 const propTypes = {
   playerNum: PropTypes.string
@@ -20,13 +22,25 @@ const Player = (props) => {
 
   const gameInfo = useContext(GameInfoContext);
   const turn = useContext(TurnContext);
+  const player = useContext(PlayerContext);
+
+  const name = (gameInfo?.[playerNum]?.name[1] !== '=')
+    ? gameInfo?.[playerNum]?.name
+    : 'Anonymous';
+
+  const css = (player === playerNum)
+    ? `${playerInfoStyle.player_text} ${playerInfoStyle.self}`
+    : playerInfoStyle.player_text;
 
   return (
     <div className={styles.container}>
       {(turn !== undefined || gameInfo?.winner) &&
         <>
           <div className={styles.info}>
-            <PlayerInfo playerNum={playerNum} />
+            <PlayerInfo
+              name={name}
+              css={css}
+            />
           </div>
           <div className={styles.result}>
             { gameInfo?.winner
