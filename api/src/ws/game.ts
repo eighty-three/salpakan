42307268050a89a4eda3f224532da075cb1e526a
Eighty Three <contact@eighty-three.dev>
2021-01-29 05:07:01 +0800

    feat: add extra key 'setup' to sent data to client
    It's only included if the player has submitted his position already. It
    persists the value of 'setup' in the client even after refreshing the
    page.

--- api/src/ws/game.ts
+++ api/src/ws/game.ts
@@ -46,10 +46,16 @@ export const open: IOpen<Promise<void>> = async (socket) => {
     gameInfo = getGameInfo(room);
     ongoingTurn = turn;
   } else {
-    gameInfo = {
-      time: room.time - Math.floor(Date.now() / 100),
-      player
-    };
+    gameInfo = (room[player].start)
+      ? {
+        time: room.time - Math.floor(Date.now() / 100),
+        player,
+        setup: true
+      } : {
+        time: room.time - Math.floor(Date.now() / 100),
+        player
+      };
+
     ongoingTurn = undefined;
   }
 
