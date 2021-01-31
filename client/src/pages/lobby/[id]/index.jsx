044202b0466031be278fd386b23e97e52d42c301
Eighty Three <contact@eighty-three.dev>
2021-01-31 08:22:22 +0800

    fix: user can't connect to the lobby immediately
    Going to a lobby created by someone else doesn't immediately connect you
    to the WebSocket if you don't have a cookie. The useCookie hook doesn't
    work; it doesn't attach the cookie immediately making you join the lobby
    on page visit even if it's the first time you visited the website (for
    example, if a friend sent them the link so they can play against each
    other).
    
    A button is added for such cases. This way, they can just "force" the
    connection. In any case, the useEffect hook is still left behind for
    users who DO have a cookie before going to the lobby.

--- client/src/pages/lobby/[id]/index.jsx
+++ client/src/pages/lobby/[id]/index.jsx
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 
 import Layout, { siteTitle } from '@/components/Layout';
 
+import styles from '@/components/Buttons.module.scss';
+
 import { lightAuthCheck } from '@/lib/authCheck';
 import useCookie from '@/lib/useCookie';
 
@@ -27,15 +29,67 @@ const LobbyPage = (props) => {
     lobbyFound
   } = props;
 
-  const initialText = 'The first person to enter this lobby will be your opponent';
-  const [ text, setText ] = useState(initialText);
+  const waitingText = 'The first person to enter this lobby will be your opponent';
+  const [text, setText] = useState('');
+
+  /* Initial state is 'true' so that the button doesn't show.
+   * It will be set to false if the socket can't connect,
+   * making the button show up
+   */
+  const [isConnected, setConnection] = useState(true);
+  const [buttonState, setButtonState] = useState({
+    disabled: true,
+    text: 'Join Lobby'
+  });
 
   useCookie(cookieValue);
 
+  // try to connect to the WS server if the user has a cookieValue
   useEffect(() => {
+    let socket;
+    if (cookieValue) {
+      socket = new WS(`${WSLOBBY_URL}/${lobbyId}`);
+
+      socket.onopen = () => {
+        setText(waitingText);
+      };
+
+      socket.onmessage = (message) => {
+        setText('Redirecting...');
+        socket.close();
+
+        const data = message.data;
+
+        Router.push(`/game/${data}`);
+      };
+    } else {
+      setConnection(false);
+    }
+
+    return () => {
+      if (cookieValue) {
+        socket.close();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    setButtonState({disabled: false, text: buttonState.text});
+  }, []);
+
+  /* Otherwise, join the lobby manually.
+   *
+   * It's a workaround because with the useCookie function, while it
+   * rerenders the component to get the cookie, I think the cookie
+   * doesn't get "loaded" to the page during useEffect's run, making
+   * it essentially useless for trying to connect the socket ASAP.
+   */
+  const onClickFn = () => {
+    setButtonState({ disabled: true, text: 'Joining...' });
+
     const socket = new WS(`${WSLOBBY_URL}/${lobbyId}`);
 
-    socket.onmessage = (message) => {
+    socket.onmessage = async (message) => {
       setText('Redirecting...');
       socket.close();
 
@@ -44,18 +98,36 @@ const LobbyPage = (props) => {
       Router.push(`/game/${data}`);
     };
 
-    return () => socket.close();
-  }, []);
+    socket.onerror = () => {
+      setButtonState({ disabled: true , text: 'Please refresh and try again' });
+    };
+  };
 
   return (
     <Layout username={username} >
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section>
+      <section className={styles.container}>
         { lobbyFound
           ? (<h1>{text}</h1>)
           : (<h1>Lobby not found</h1>)
+        }
+
+        {/* Only show the button if there's a lobby found
+          * and if the socket is not connected. It prevents
+          * the button from showing when there's no actual
+          * lobby and if the user is already connected (so
+          * he doesn't have to join
+          */}
+        {(isConnected === false) && lobbyFound &&
+          <button
+            onClick={onClickFn}
+            disabled={buttonState.disabled}
+            className={styles.button}
+          >
+            {buttonState.text}
+          </button>
         }
       </section>
     </Layout>
