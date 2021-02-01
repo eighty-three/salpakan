6cdfa8e52aeee9fd720cac86d1deb7862b06148a
Eighty Three <contact@eighty-three.dev>
2021-02-02 06:43:05 +0800

    fix: rework lobbies
    BREAKING CHANGE: changed tables in the database
    
    Leave cleanup to cronjob instead of deleting on close. It allows
    players to rejoin the lobby instead of immediately deleting it once the
    player closes the tab.
    
    In order to make the cleanup work, add an expiry. This way, the lobby
    can persist for a short while. When querying the lobby, just return the
    lobbies that haven't expired yet. On socket close, just remove the
    socket from the connections array.
    
    However, if someone else joins the lobby to start the match, delete the
    lobby both in memory and in the database since it's not needed anymore

--- api/src/lobby/model.ts
+++ api/src/lobby/model.ts
@@ -5,11 +5,26 @@ export const createLobby = async (
   roomName: string,
 ): Promise<void> => {
   const query = new PS({ name: 'create-lobby', text: '\
-    INSERT INTO lobbies (name) VALUES ($1)'
+    INSERT INTO lobbies (name, expiry) VALUES ($1, $2)'
   });
 
-  query.values = [ roomName ];
+  const expiry = Math.floor(Date.now() / 1000) + 600; // 10 minutes
+
+  query.values = [ roomName, expiry ];
   await db.none(query);
+};
+
+export const getLobby = async (
+  roomName: string,
+): Promise<string|null> => {
+  const query = new PS({ name: 'get-lobby', text: '\
+    SELECT name FROM lobbies WHERE name=$1 AND expiry > $2'
+  });
+
+  const currentTime = Math.floor(Date.now() / 1000);
+
+  query.values = [ roomName, currentTime ];
+  return await db.oneOrNone(query);
 };
 
 export const deleteLobby = async (
@@ -21,15 +36,4 @@ export const deleteLobby = async (
 
   query.values = [ roomName ];
   await db.none(query);
-};
-
-export const getLobby = async (
-  roomName: string,
-): Promise<string|null> => {
-  const query = new PS({ name: 'get-lobby', text: '\
-    SELECT name FROM lobbies WHERE name=$1'
-  });
-
-  query.values = [ roomName ];
-  return await db.oneOrNone(query);
 };
--- api/src/ws/lobby.ts
+++ api/src/ws/lobby.ts
@@ -7,24 +7,28 @@ import { getUsername } from '@authMiddleware/authToken';
 import { startGame } from '../game/model';
 import { deleteLobby } from '../lobby/model';
 
-interface IConnections {
-  [key: string]: string[];
+interface ILobbies {
+  [key: string]: {
+    connections: string[];
+  }
 }
 
-const connections: IConnections = {};
+export const lobbies: ILobbies = {};
 
 export const upgrade: IUpgrade<Promise<void>> = async (res, req, context) => {
   const upgradeAborted = {aborted: false};
   const cn = await getUsername(req.getHeader('cookie'));
   const url = req.getParameter(0);
 
-  if (!connections[url]) {
-    connections[url] = [];
+  if (!lobbies[url]) {
+    lobbies[url] = {
+      connections: [],
+    };
   }
 
   if (
     cn
-    && !connections[url].includes(cn)
+    && !lobbies[url].connections.includes(cn)
     && req.getHeader('origin') === config.CLIENT_HOST
   ) {
     res.upgrade(
@@ -42,18 +46,24 @@ export const upgrade: IUpgrade<Promise<void>> = async (res, req, context) => {
 };
 
 export const open: IOpen<Promise<void>> = async (socket) => {
-  connections[socket.url].push(socket.cn);
+  lobbies[socket.url].connections.push(socket.cn);
   socket.subscribe(`${socket.url}_pl`);
 
-  if (connections[socket.url].length > 1) {
-    await startGame(gameStates, socket.url, connections[socket.url]);
+  if (lobbies[socket.url].connections.length > 1) {
+    // start the game
+    await startGame(gameStates, socket.url, lobbies[socket.url].connections);
+
+    // and delete it in memory and in the database
+    delete lobbies[socket.url];
+    await deleteLobby(socket.url);
 
     socket.publish(`${socket.url}_pl`, socket.url);
   }
 };
 
-export const close: IClose<void> = async (socket) => {
-  delete connections[socket.url];
-  await deleteLobby(socket.url);
+export const close: IClose<void> = (socket) => {
+  if (lobbies[socket.url]) {
+    const toDelete = lobbies[socket.url].connections.indexOf(socket.cn);
+    if (toDelete > -1) lobbies[socket.url].connections.splice(toDelete, 1);
+  }
 };
-
