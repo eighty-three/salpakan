1a463fa361180c5f05c35e35b48a0b8c06008356
Eighty Three <contact@eighty-three.dev>
2021-02-02 06:47:56 +0800

    feat: add deleteLobbies for cleanup
    Add it inside the current cronjob for the expired games

--- api/src/ws/index.ts
+++ api/src/ws/index.ts
@@ -82,17 +82,31 @@ export default wsApp;
 
 
 import cron from 'node-cron';
-import { deleteGames } from '../game/model';
-import { IName } from '../game/types';
 
-const task = cron.schedule('*/30 * * * *', async () => {
-  const deletedGames: IName[] | null = await deleteGames();
+import { deleteGames } from '../game/model';
+import { IName as gameNames } from '../game/types';
+
+import { deleteLobbies } from '../lobby/model';
+import { IName as lobbyNames } from '../lobby/types';
+import { lobbies } from '../ws/lobby';
+
+//every 30 minutes
+const cleanup = cron.schedule('*/30 * * * *', async () => {
+  const deletedGames: gameNames[] | null = await deleteGames();
   if (deletedGames?.length) {
     console.log(`Deleting ${deletedGames.length} game(s)...`);
     for (let i=0; i < deletedGames.length; i++) {
       delete gameStates[deletedGames[i].name];
     }
   }
+
+  const deletedLobbies: lobbyNames[] | null = await deleteLobbies();
+  if (deletedLobbies?.length) {
+    console.log(`Deleting ${deletedLobbies.length} lobby(s????)...`);
+    for (let i=0; i < deletedLobbies.length; i++) {
+      delete lobbies[deletedLobbies[i].name];
+    }
+  }
 });
 
-task.start();
+cleanup.start();
