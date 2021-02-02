02cedc7d806cdbc48f4f668880a61914374a8be9
Eighty Three <contact@eighty-three.dev>
2021-02-03 07:41:19 +0800

    fix: force refresh if AFK
    With how it currently works, the cronjob deletes the expired games both
    in memory and in the database. The problem is that as long as the user
    is in the page, the status indicator will keep on doing its job,
    querying the WS '/ingameCount' endpoint. While the 'AFK' case doesn't
    delete the game state in memory immediately anymore, if the player stays
    on the page, it'll just crash the server again. Forcing a refresh seems
    to be the way to go for now.
    
    There definitely should be something added to the status indicator to
    prevent it from querying if the game has ended already.

--- client/src/components/Game/Setup.jsx
+++ client/src/components/Game/Setup.jsx
@@ -5,12 +5,36 @@ import Countdown from './MatchClock/Countdown';
 
 import useButton from '@/hooks/useButton';
 import GameStateContext from '@/contexts/GameStateContext';
+import useDelay from '@/hooks/useDelay';
 
 const Setup = () => {
   const [gameState, dispatch] = useContext(GameStateContext);
-  const [ time, setTime ] = useState(gameState.gameInfo?.time);
-  const [ afk, setAfk ] = useState(false);
+  const [time, setTime] = useState(gameState.gameInfo?.time);
+  const [afk, setAfk] = useState(false);
   const [buttonState, setButtonState] = useButton('Submit Board');
+  const [delay, clearDelay] = useDelay(3);
+
+  useEffect(() => {
+    // If afk
+    if (afk) {
+      const fn = async () => {
+
+        // Wait 3 seconds
+        await delay();
+
+        // then refresh the page
+        if (typeof window !== undefined) {
+          window.location.reload();
+        }
+      };
+
+      fn();
+    }
+
+    return () => {
+      clearDelay();
+    };
+  }, [afk]);
 
   useEffect(() => {
     setTime(gameState.gameInfo?.time);
@@ -40,21 +64,25 @@ const Setup = () => {
 
   return (
     <div className={styles.container}>
-      { gameState.gameInfo &&
-        <>
-          <button className={styles.button} onClick={onClickFn} disabled={buttonState.disabled}>
-            {buttonState.text}
-          </button>
-          <div className={styles.clock}>
-            <Countdown time={time} counter={countDown} />
+      { afk
+        ? (
+          <div className={styles.afk}>
+            Your opponent was AFK, find another match
           </div>
-        </>
-      }
-
-      { afk &&
-        <div className={styles.afk}>
-          Your opponent was AFK, find another match
-        </div>
+        ) : (
+          <>
+            { gameState.gameInfo &&
+              <>
+                <button className={styles.button} onClick={onClickFn} disabled={buttonState.disabled}>
+                  {buttonState.text}
+                </button>
+                <div className={styles.clock}>
+                  <Countdown time={time} counter={countDown} />
+                </div>
+              </>
+            }
+          </>
+        )
       }
     </div>
   );
