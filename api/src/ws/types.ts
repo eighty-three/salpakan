b490ff6ee155ba1afba4f621956b64eb079dc1dd
Eighty Three <contact@eighty-three.dev>
2021-02-11 00:57:29 +0800

    feat: add new types, place others into the file
    Instead of having it all over the place in 'ws/utils.ts' and other files
    in the directory. It was fine before when it was just one or two types
    added beside the function but now it's too cluttered.

--- api/src/ws/types.ts
+++ api/src/ws/types.ts
@@ -5,6 +5,7 @@ import {
   WebSocket
 } from 'uWebSockets.js';
 
+// WebSocket methods
 export interface IUpgrade<T> {
   (
     res: HttpResponse,
@@ -25,4 +26,41 @@ export interface IMessage<T> {
     socket: WebSocket,
     message: ArrayBuffer
   ): T
+}
+
+
+import { TPlayer, IPlayer } from '../game/types';
+
+type TPartialPlayers = { [K in TPlayer]: Pick<IPlayer, 'name' | 'time'> };
+export interface IGameInfo extends TPartialPlayers {
+  winner: string | null;
+  connections: string[];
+  bot: boolean;
+}
+
+export enum WS_RESPONSE_CODE {
+  CONTINUE = 4000,
+  GAME_NOT_FOUND,
+  IN_SETUP
+}
+
+export interface IResponse {
+  code: WS_RESPONSE_CODE,
+  reason?: string
+}
+
+export interface IHandshake {
+  req: HttpRequest,
+  res: HttpResponse,
+  context: us_listen_socket
+}
+
+export interface ILobbies {
+  [key: string]: {
+    connections: string[];
+  }
+}
+
+export interface IConnections {
+  [key: string]: string[];
 }
