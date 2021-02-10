22ba46a49082e1ddb86624ae13619dd1bb8ed456
Eighty Three <contact@eighty-three.dev>
2021-02-11 01:16:00 +0800

    feat: force game to start on rematch if bot

--- api/src/ws/rematch.ts
+++ api/src/ws/rematch.ts
@@ -1,20 +1,14 @@
-import { gameStates } from './index';
-import config from '@utils/config';
 import { nanoid } from 'nanoid';
-
-import { IUpgrade, IOpen, IClose } from './types';
-
+import config from '@utils/config';
 import { getUsername } from '@authMiddleware/authToken';
+import { gameStates } from './index';
 import { startGame } from '../game/model';
-
-interface IConnections {
-  [key: string]: string[];
-}
+import { handshakeHandler } from './utils';
+import { IUpgrade, IOpen, IClose, IConnections } from './types';
 
 const connections: IConnections = {};
 
 export const upgrade: IUpgrade<Promise<void>> = async (res, req, context) => {
-  const upgradeAborted = { aborted: false };
   const cn = await getUsername(req.getHeader('cookie'));
   const url = req.getParameter(0);
 
@@ -22,37 +16,32 @@ export const upgrade: IUpgrade<Promise<void>> = async (res, req, context) => {
     connections[url] = [];
   }
 
-  if (
-    cn
-    && !connections[url].includes(cn)
-    && req.getHeader('origin') === config.CLIENT_HOST
-  ) {
-    res.upgrade(
-      { cn, url },
-      req.getHeader('sec-websocket-key'),
-      req.getHeader('sec-websocket-protocol'),
-      req.getHeader('sec-websocket-extensions'),
-      context
-    );
-  } else {
-    res.onAborted(() => { upgradeAborted.aborted = true; });
-    res.writeStatus('400 Bad Request');
-    res.end();
-  }
+  const checks = [
+    !!cn,
+    !!(cn && !connections[url].includes(cn)),
+    (req.getHeader('origin') === config.CLIENT_HOST)
+  ];
+  const handshake = { req, res, context };
+  handshakeHandler({ cn, url }, checks, handshake);
 };
 
 export const open: IOpen<Promise<void>> = async (socket) => {
-  connections[socket.url].push(socket.cn);
   socket.subscribe(`${socket.url}_rm`);
+  const newUrl = nanoid(10);
 
+  if (gameStates[socket.url].bot) {
+    await startGame(gameStates, newUrl, [socket.cn, 'Mr. Bot'], true);
+    socket.publish(`${socket.url}_rm`, newUrl);
+    return;
+  }
+
+  connections[socket.url].push(socket.cn);
   if (connections[socket.url].length > 1) {
-    const newUrl = nanoid(10);
     await startGame(gameStates, newUrl, connections[socket.url]);
-
     socket.publish(`${socket.url}_rm`, newUrl);
   }
 };
 
-export const close: IClose<void> = async (socket) => {
+export const close: IClose<void> = (socket) => {
   delete connections[socket.url];
 };
