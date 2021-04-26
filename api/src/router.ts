d3d335ebd5719bd95a1f50f0df673eb440d01033
Eighty Three <contact@eighty-three.dev>
2021-04-26 10:08:10 +0800

    fix: add auth endpoint

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,25 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
-// Middleware
-import { authToken, authAccount } from '@authMiddleware/index';
-import Joi from '@hapi/joi';
-import validator from '@utils/validator';
-
-const username = Joi.object({
-  username: Joi.string().regex(/^[a-zA-Z0-9_]{1,29}$/).required(),
-});
-
-router.post('/verify',
-  validator(username, 'body'),
-  authToken.verifyToken,
-  authAccount.verifyUser
-);
-
-
 // Routes
 import accountRouter from './account/router';
 router.use('/account', accountRouter);
+
+import authRouter from './auth/router';
+router.use('/auth', authRouter);
 
 import gameRouter from './game/router';
 router.use('/game', gameRouter);
